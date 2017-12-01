c8d418f2051d35459a32087bba9c44dbd800a148
Alex Ihnatsiuk <ihnatsiuka@gmail.com>
2017-12-01 09:07:39 -0500

    First test

---
+++ java-example/src/test/java/MyFirstTest.java
@@ -0,0 +1,35 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
+
+public class MyFirstTest {
+
+    private WebDriver driver;
+    private WebDriverWait wait;
+
+    @Before
+    public void start () {
+        System.setProperty("webdriver.chrome.driver", "C:\\Users\\Alex-Work\\Desktop\\Work\\chromedriver.exe");
+        driver = new ChromeDriver();
+        wait = new WebDriverWait(driver, 10);
+    }
+
+    @Test
+    public void myFirstTest() {
+        driver.get("https://www.google.com/");
+
+
+    }
+
+  @After
+    public void stop(){
+        driver.quit();
+        driver = null;
+    }
+}
